a552378d948ef45d4414d01600320d655ae0dcb0
Bishru182 <bishrumohammed182@gmail.com>
2025-01-28 11:23:43 +0530

    delete function complete

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -18,6 +18,16 @@ function Home() {
         })
         .catch(err => console.log(err))
     }
+
+    const handleDelete = (id) => {
+        axios.delete('http://localhost:3001/delete/'+id)
+        .then(result => {
+            location.reload()
+        })
+        .catch(err => console.log(err))
+
+    }
+
   return (
     <div className='home'>
         <h2>Todo List</h2>
@@ -37,7 +47,8 @@ function Home() {
                           <p className={todo.done ? "line_through" : ""}>{todo.task}</p>
                      </div>
                      <div>
-                        <span><BsFillTrashFill className='icon'/></span>
+                        <span><BsFillTrashFill className='icon' 
+                             onClick={() =>handleDelete(todo._id)}/></span>
                      </div>
                 </div>
             ))
